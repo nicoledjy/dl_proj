0f5c0eae24aaeb465c6fd4f55edec7e3828d9639
nicoledjy <jd4138@nyu.edu>
2020-04-29 14:13:47 -0400

    upload scripts

---
+++ pretrain/sbatch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+#SBATCH --time=24:00:00
+#SBATCH --mem=16GB
+#SBATCH --job-name=pretrain
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jd4138@nyu.edu
+#SBATCH --output=slurm_%j.out
+
+
+
+cd /scratch/jd4138/dl_proj/pretrain
+
+. ~/.bashrc
+
+module load cudnn/10.1v7.6.5.32
+module load cuda/10.1.105
+
+conda activate pytorch
+conda install -n pytorch nb_conda_kernels
+
+python3 pirl_train.py --num-scene 106 --model-type res50 --batch-size 128 --lr 0.005 --count-negatives 40000
