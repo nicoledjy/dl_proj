6cbd9b19bb0ee8c473a4444e7278e4359ae7b580
nicoledjy <jd4138@nyu.edu>
2020-04-29 14:57:15 -0400

    modify script

--- pretrain/sbatch.s
+++ pretrain/sbatch.s
@@ -12,14 +12,15 @@
 
 
 
-cd /scratch/jd4138/dl_proj/pretrain
+
 
 . ~/.bashrc
-
+module load anaconda3/5.3.1
 module load cudnn/10.1v7.6.5.32
 module load cuda/10.1.105
 
 conda activate pytorch
-conda install -n pytorch nb_conda_kernels
 
+
+cd /scratch/jd4138/dl_proj/pretrain
 python3 pirl_train.py --num-scene 106 --model-type res50 --batch-size 128 --lr 0.005 --count-negatives 40000
