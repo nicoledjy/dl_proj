cb214611a6e05ef7f27fdd895637dd4c01b64503
nicoledjy <jd4138@nyu.edu>
2020-04-29 15:27:26 -0400

    update

--- pretrain/sbatch.s
+++ pretrain/sbatch.s
@@ -15,13 +15,14 @@
 
 
 . ~/.bashrc
-module load anaconda3/5.3.1
-module load cudnn/10.1v7.6.5.32
-module load cuda/10.1.105
+
+module purge
+module load cuda/10.0.130
+module load cudnn/10.0v7.4.2.24
 
 conda activate pytorch
 
 
 cd /scratch/jd4138/dl_proj/pretrain
-import torch
-python3 pirl_train.py --num-scene 106 --model-type res50 --batch-size 128 --lr 0.005 --count-negatives 40000
+
+python pirl_train.py --num-scene 106 --model-type res50 --batch-size 128 --lr 0.005 --count-negatives 40000
